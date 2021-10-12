0d13a374642aa6f6055b25f9fe9cc1e1321553af
Ahadvt <ahadvt@code.edu.az>
2021-10-12 05:31:53 +0400

    Add project files.

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedList<int, string> sortedlist = new SortedList<int, string>();
+
+            sortedlist.Add(1995, "java");
+            sortedlist.Add(1972, "c#");
+            sortedlist.Add(1990, "python");
+
+            foreach (var item in sortedlist)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
